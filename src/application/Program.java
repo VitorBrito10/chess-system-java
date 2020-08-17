466309d9f1490576cedfe6551c0a8329b5231fa2
Vitor Brito <vitor.b.araujo10@gmail.com>
2020-08-17 15:56:00 -0300

    Implementing possible moves of Rook

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,33 +11,32 @@ import chess.ChessPosition;
 public class Program {
 
 	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
+		
+		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
-
+		
 		while (true) {
 			try {
 				UI.clearScreen();
 				UI.printBoard(chessMatch.getPieces());
 				System.out.println();
 				System.out.print("Source: ");
-				ChessPosition source = UI.readChessPosition(scan);
-
+				ChessPosition source = UI.readChessPosition(sc);
+				
 				System.out.println();
 				System.out.print("Target: ");
-				ChessPosition targe = UI.readChessPosition(scan);
-
-				ChessPiece capturedPiece = chessMatch.performChessMove(source, targe);
-			} catch (ChessException e) {
-				System.out.println(e.getMessage());
-				scan.nextLine();
+				ChessPosition target = UI.readChessPosition(sc);
+				
+				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
 			}
-
+			catch (ChessException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
 			catch (InputMismatchException e) {
 				System.out.println(e.getMessage());
-				scan.nextLine();
+				sc.nextLine();
 			}
 		}
-
 	}
-
 }
